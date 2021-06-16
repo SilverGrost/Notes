40a925ccd6f9e289ffb0a2f3eaa6c6699e85df67
SilverGrost <silvergrost@silcomsoft.ru>
2021-06-16 15:12:33 +0300

    Добавил виджет поиска в верхнее меню

--- app/src/main/java/ru/geekbrains/notes/ui/MainActivity.java
+++ app/src/main/java/ru/geekbrains/notes/ui/MainActivity.java
@@ -2,6 +2,7 @@ package ru.geekbrains.notes.ui;
 
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,7 +13,10 @@ import androidx.fragment.app.FragmentTransaction;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -161,6 +165,49 @@ public class MainActivity extends AppCompatActivity implements ListNotesFragment
         setSupportActionBar(toolbar);
         return toolbar;
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Обработка выбора пункта меню приложения (активити)
+        int id = item.getItemId();
+
+        /*switch(id){
+            case R.id.action_settings:
+                addFragment(new SettingsFragment());
+                return true;
+            case R.id.action_main:
+                addFragment(new MainFragment());
+                return true;
+            case R.id.action_favorite:
+                addFragment(new FavoriteFragment());
+                return true;
+        }*/
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Здесь определяем меню приложения (активити)
+        getMenuInflater().inflate(R.menu.main, menu);
+        MenuItem search = menu.findItem(R.id.action_search); // поиск пункта меню поиска
+        SearchView searchText = (SearchView) search.getActionView(); // строка поиска
+        searchText.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            // реагирует на конец ввода поиска
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Toast.makeText(MainActivity.this, query, Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            // реагирует на нажатие каждой клавиши
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return true;
+            }
+        });
+
+        return true;
+    }
+
 
     @Override
     public void onNoteClickedList(int noteId) {
